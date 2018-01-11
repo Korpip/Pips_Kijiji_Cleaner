4f7ea2f22eda112bfb528fbbaca46d3cddb3320f
Korpip <thomas.shawn.milne@gmail.com>
2018-01-11 18:09:35 -0500

    Create Pips_Kijiji_Cleaner.js

---
+++ Pips_Kijiji_Cleaner.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        Pips Kiji Cleaner
+// @namespace   namespace
+// @version     1.4
+// @include     https://*.kijiji.ca/*
+// @include     http://*.kijiji.ca/*
+// @version     1
+// @grant       none
+// @require http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// ==/UserScript==
+$('.top-feature').remove();
+$('.third-party').remove();
